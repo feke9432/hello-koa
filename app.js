8c312a2d7a6b8a67883445a20ccc83d90cb3e3ee
feke9432 <feke9432@gmail.com>
2017-11-02 21:11:04 +0800

    ss

--- app.js
+++ app.js
@@ -1,47 +1,20 @@
-const Koa = require('koa');
+var koa = require('koa');
 
-const router = require('koa-router')(); // 这里返回的是函数。
+var bodyParser = require('koa-bodyparser')
 
-const bodyParser = require('koa-bodyparser'); // 解析post请求中的 body 数据。
+const controller = require('./constrollers');
 
-const app = new Koa();
+var app = new koa();
 
-// ctx => { request , response }
-app.use(async (ctx, next) => {
+app.use(async(ctx, next) => {
     console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
     await next();
 });
 
-router.get('/page/:name', async(ctx, next) => {
-    var name = ctx.params.name;
-    ctx.response.body = `<h1>hello ${name}!</h1>`;
-});
+app.use(bodyParser());
 
-router.get('/', async(ctx, next) => {
-    ctx.response.body = `<h1>Index</h1>\
-    <form action="/signin" method="post">\
-        <p>Name: <input name="name" value="koa"></P>\
-        <p>Password: <input name="password" type="password"></p>\
-        <p><input type="submit" value="Submit"></p>\
-    </form>`;
-});
-
-router.post('/signin', async(ctx, next) => {
-    var name = ctx.request.body.name || '';
-    var password = ctx.request.body.password || '';
-    console.log(`signin with name: ${name} , password: ${password}`);
-    if (name === 'admin' && password === '12345') {
-        ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
-    } else {
-        ctx.response.body = '<h1>Login failed!</h1>\
-        <p><a href="/">Try again</a></p>';
-    }
-});
-
-app.use(bodyParser()); // 必须在路由之前。
-
-app.use(router.routes());
+app.use(controller());
 
 app.listen(3000);
 
-console.log('app start at port 3000');
+console.log('app is running at port 3000');
